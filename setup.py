4590ae21490e51136a393cf20838957458bbc9fe
Sasha Nenudny <sasha.nenudny@gmail.com>
2018-04-04 13:46:53 +0300

    Like python package now

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+import glob
+from setuptools import setup
+from setuptools import find_packages
+
+APP_NAME = "ivoicer"
+
+requirements = [
+    "chardet"
+]
+
+setup(
+    name=APP_NAME,
+    version='2.0a0',
+    packages=find_packages(),
+    url='https://github.com/0valse/voiceover/',
+    license='GPL-3',
+    author='Sasha Nenudny',
+    author_email='sasha.nenudny@gmail.com',
+    description='Alpha version',
+    long_description="Voice-over of the text in Russian with Yandex SpeechKit",
+    classifiers = (
+        'Development Status :: 3 - Alfa',
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        'Operating System :: POSIX :: Linux',
+        'Natural Language :: English',
+        'Natural Language :: Russian',
+        ),
+    scripts=['scripts/ivoicer'],
+    install_requires=requirements,
+)
